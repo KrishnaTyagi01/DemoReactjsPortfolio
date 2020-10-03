160c456e62bc2b401ced7ec140e2ed99161d01b8
krishna <tyagikrishna38@gmail.com>
2020-10-03 14:18:59 +0530

    Updates

---
+++ precache-manifest.98f79208f68af46abd9c02c91e839780.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f2788540dccf88dc5d3ebbfe0aa7dda",
+    "url": "/DemoReactjsPortfolio/index.html"
+  },
+  {
+    "revision": "e6dffb1962f3ecfc3f2b",
+    "url": "/DemoReactjsPortfolio/static/css/main.c125a653.chunk.css"
+  },
+  {
+    "revision": "a96526bf832a8132caca",
+    "url": "/DemoReactjsPortfolio/static/js/2.442e77f1.chunk.js"
+  },
+  {
+    "revision": "6c9b04a1bdde37a6e9b709806c14aec8",
+    "url": "/DemoReactjsPortfolio/static/js/2.442e77f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6dffb1962f3ecfc3f2b",
+    "url": "/DemoReactjsPortfolio/static/js/main.6ef0dd38.chunk.js"
+  },
+  {
+    "revision": "523f864a4e36a9f57434",
+    "url": "/DemoReactjsPortfolio/static/js/runtime-main.7e34a20c.js"
+  },
+  {
+    "revision": "8240d16d2e4238d726374585da548f23",
+    "url": "/DemoReactjsPortfolio/static/media/MyPic.8240d16d.jpeg"
+  },
+  {
+    "revision": "b3bee91ab413a3ac5484893cc6530477",
+    "url": "/DemoReactjsPortfolio/static/media/logo.b3bee91a.png"
+  }
+]);
